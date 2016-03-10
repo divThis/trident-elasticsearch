776821ca91a0e2ac83f9ea3f4902b6b239496378
Christopher M. Mitchell <christopher@divthis.com>
2016-03-10 09:35:41 -0800

    Updating to ElasticSearch 2.2.0

--- src/main/java/com/hmsonline/storm/es/trident/TransportClientFactory.java
+++ src/main/java/com/hmsonline/storm/es/trident/TransportClientFactory.java
@@ -1,12 +1,14 @@
 package com.hmsonline.storm.es.trident;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,14 +23,14 @@ public class TransportClientFactory implements ClientFactory {
     @Override
     @SuppressWarnings("rawtypes")
     public Client makeClient(Map conf) {
-        ImmutableSettings.Builder settingsBuilder = ImmutableSettings.settingsBuilder();
+        Settings.Builder settingsBuilder = Settings.builder();
         settingsBuilder.put("cluster.name", (String) conf.get(CLUSTER_NAME));
         
         synchronized (MUTEX) {
             String clusterHosts = (String) conf.get(CLUSTER_HOSTS);
             TransportClient client = CLIENTS.get(clusterHosts);
             if (client == null) {
-                client = new TransportClient(settingsBuilder);
+                client = TransportClient.builder().settings(settingsBuilder).build();
 
                 LOG.info("Creating TransportClient with addresses: '{}'", clusterHosts);
 
@@ -37,11 +39,20 @@ public class TransportClientFactory implements ClientFactory {
                     String[] hostPorts = StringUtils.split(clusterHosts, ",");
                     for (String hostPortStr : hostPorts) {
                         String[] hostPort = StringUtils.split(hostPortStr, ":");
+
+                        InetAddress address;
+
+                        try {
+                            address = InetAddress.getByName(hostPort[0]);
+                        } catch (UnknownHostException e) {
+                            throw new IllegalStateException("Unknown host: '" + hostPort[0], e);
+                        }
+
                         if (hostPort.length == 2) {
-                            client.addTransportAddress(new InetSocketTransportAddress(hostPort[0], Integer
+                            client.addTransportAddress(new InetSocketTransportAddress(address, Integer
                                     .parseInt(hostPort[1])));
                         } else if (hostPort.length == 1) {
-                            client.addTransportAddress(new InetSocketTransportAddress(hostPort[0], DEFAULT_PORT));
+                            client.addTransportAddress(new InetSocketTransportAddress(address, DEFAULT_PORT));
                         }
                     }
 
